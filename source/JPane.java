95b65df9fe712150d7814da20cf5a50edde9bec0
PauloHeitor <paulo.carrari@ifood.com.br>
2021-07-19 10:53:55 -0300

    JOptionPane movido do Java.java  para o arquivo JPane.java

---
+++ source/JPane.java
@@ -0,0 +1,11 @@
+import javax.swing.JOptionPane;
+
+class JPane{
+    public static void main(String[] args){
+        System.out.println("Hello World!");
+
+        JOptionPane.showMessageDialog(null,"Hello World!");
+
+    }
+
+    }
