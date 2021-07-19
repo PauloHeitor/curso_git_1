0dd324cc215f41e5254f543b19858825d69cbcea
PauloHeitor <paulo.carrari@ifood.com.br>
2021-07-19 10:36:18 -0300

    Joption frame add no java

--- source/Java.java
+++ source/Java.java
@@ -1,5 +1,11 @@
+import javax.swing.JOptionPane;
+
 class Java{
     public static void main(String[] args){
         System.out.println("Hello World!");
+
+        JOptionPane.showMessageDialog(null,"Teste");
+
     }
-}
+
+    }
