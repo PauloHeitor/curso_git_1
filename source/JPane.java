6ddcf1347ca59447b58b545579d05f4f23a02905
PauloHeitor <paulo.carrari@ifood.com.br>
2021-07-20 12:03:36 -0300

    add novo JPanel

--- source/JPane.java
+++ source/JPane.java
@@ -4,7 +4,11 @@ class JPane{
     public static void main(String[] args){
         System.out.println("Hello World!");
 
-        JOptionPane.showMessageDialog(null,"Hello World!");
+        String txt = "Paulo Heitor";
+
+        JOptionPane.showInputDialog("Digite seu Nome:", txt);
+
+        JOptionPane.showMessageDialog(null,"Hello " + txt);
 
     }
 
